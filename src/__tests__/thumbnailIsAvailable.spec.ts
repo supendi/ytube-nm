05b3a64bbd9d13d18631f96c4ebda052ea13dcea
supendi <irpan.supendi@gmail.com>
2021-04-22 12:14:29 +0700

    thumbnailIsAvailable.spec test

--- src/__tests__/thumbnailIsAvailable.spec.ts
+++ src/__tests__/thumbnailIsAvailable.spec.ts
@@ -11,10 +11,21 @@ describe("Thumbnail is available test", () => {
             const actual = await thumbnailIsAvailable(url)
             expect(actual).toEqual(expected)
         }
-
     });
 
-    test("it should return null", () => {
-
+    test("it should contains unavailable thumbnail", async () => {
+        const videoId = "bE8sO0TG9CE" //padi begitu indah doesnt have all resolution types
+        const youtubeUrl = `https://www.youtube.com/watch?v=${videoId}`
+        const urls = getAllYoutubeThumbnailURLs(youtubeUrl)
+        const result = []
+        for (let index = 0; index < urls.length; index++) {
+            const url = urls[index];
+            const isAvailable = await thumbnailIsAvailable(url)
+            // console.log(url + " does have thumbnail:", isAvailable)
+            result.push(isAvailable)
+        }
+        const actual = result.includes(false)
+        const expected = true
+        expect(actual).toEqual(expected)
     });
 });
