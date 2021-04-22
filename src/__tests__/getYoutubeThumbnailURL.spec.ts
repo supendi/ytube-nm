a31ff22b0a905f8f402ecce8f6ae8020a96767c2
supendi <irpan.supendi@gmail.com>
2021-04-22 11:50:02 +0700

    getYoutubeThumbnailURL test

--- src/__tests__/getYoutubeThumbnailURL.spec.ts
+++ src/__tests__/getYoutubeThumbnailURL.spec.ts
@@ -9,7 +9,7 @@ const resTypes = [
 ]
 
 describe("Get single thumbnail by resolution type test", () => {
-    test("it should return hqdefault thumbnail", () => {
+    test("it should return correct thumbnail url", () => {
         const videoId = "6XBpQvFEfTk"
         const youtubeUrl = `https://www.youtube.com/watch?v=${videoId}`
 
@@ -20,5 +20,17 @@ describe("Get single thumbnail by resolution type test", () => {
             expect(actual).toEqual(expected)
         }
 
+    });
+
+    test("it should null", () => {
+        const videoId = "6XBpQvFEfTk"
+        const youtubeUrl = `https://www.anime.com/watch?v=${videoId}`
+
+        for (let index = 0; index < resTypes.length; index++) {
+            const resType = resTypes[index] as RES_TYPE;
+            const actual = getYoutubeThumbnailURL(youtubeUrl, resType)
+            const expected = null
+            expect(actual).toEqual(expected)
+        }
     });
 });
