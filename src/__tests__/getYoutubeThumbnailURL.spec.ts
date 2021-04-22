3a853c27aab8d9c2d8cbdd62021bad08e111bed0
supendi <irpan.supendi@gmail.com>
2021-04-22 11:36:11 +0700

    getYoutubeThumbnailURLs test

---
+++ src/__tests__/getYoutubeThumbnailURL.spec.ts
@@ -0,0 +1,21 @@
+import { getYoutubeThumbnailURL } from '../index'
+
+const imageTypes = [
+    "hqdefault.jpg",
+    "mqdefault.jpg",
+    "sddefault.jpg",
+    "maxresdefault.jpg",
+    "mqdefault.jpg"
+]
+
+describe("Get single thumbnail by resolution type test", () => {
+    test("it should return hqdefault thumbnail", () => {
+        const videoId = "6XBpQvFEfTk"
+        const youtubeUrl = `https://www.youtube.com/watch?v=${videoId}`
+        const actual = getYoutubeThumbnailURL(youtubeUrl)
+
+        const expected = `https://img.youtube.com/vi/${videoId}/hqdefault.jpg`
+
+        expect(actual).toEqual(expected)
+    });
+});
