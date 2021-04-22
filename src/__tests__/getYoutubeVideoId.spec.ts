966078ac3fa50bc42a30490e190d3d763c24e5de
supendi <irpan.supendi@gmail.com>
2021-04-22 10:45:38 +0700

    getYoutubeSplitter.spec test

--- src/__tests__/getYoutubeVideoId.spec.ts
+++ src/__tests__/getYoutubeVideoId.spec.ts
@@ -1,16 +1,16 @@
 import { getYoutubeVideoId } from '../index'
 describe("Get video id function", () => {
     test("it should return youtube id", () => {
-        // const youtbeURL = "https://www.youtube.com/watch?v=6XBpQvFEfTk"
-        // const expectedVideoId = "6XBpQvFEfTk"
-        // const actualVideoId = getYoutubeVideoId(youtbeURL)
-        // expect(actualVideoId).toEqual(expectedVideoId)
-    });
-
-    test("it should return false youtube id", () => {
-        // const youtbeURL = "https://www.google.com/search?q=jest+unit+test&safe=strict&rlz=1C1ONGR_enID930ID930&sxsrf=ALeKk030w5NbNQaUefJ7yLsd480xUtzgvQ%3A1619054724400&ei=hNCAYKH9F9K9rQHyuLOoAw&oq=jest+unit+test&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBMgUIABDLATIFCAAQywEyBQgAEMsBOgQIIxAnOgYIIxAnEBM6BAgAEEM6CAgAELEDEIMBOggILhCxAxCDAToFCC4QsQM6CggAELEDEIMBEEM6BQgAELEDOgIIAFDtxQ5YqtYOYLPXDmgAcAJ4AIABU4gBkgeSAQIxNZgBAKABAaoBB2d3cy13aXrAAQE&sclient=gws-wiz&ved=0ahUKEwihz-Dt2JDwAhXSXisKHXLcDDUQ4dUDCA4&uact=5"
-        // const expectedVideoId = null
-        // const actualVideoId = getYoutubeVideoId(youtbeURL)
-        // expect(actualVideoId).toEqual(expectedVideoId)
+        const urls = [
+            "https://www.youtube.com/watch?v=6XBpQvFEfTk",
+            "https://youtu.be/6XBpQvFEfTk",
+            "https://www.youtube.com/embed/6XBpQvFEfTk"
+        ]
+        for (let index = 0; index < urls.length; index++) {
+            const url = urls[index];
+            const expectedVideoId = "6XBpQvFEfTk"
+            const actualVideoId = getYoutubeVideoId(url)
+            expect(actualVideoId).toEqual(expectedVideoId) 
+        } 
     });
 });
